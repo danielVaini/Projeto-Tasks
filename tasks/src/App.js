5e6c988d5e24abbc3b2eaff0db4c4e586fe63ba2
Daniel Santos <dnlvaini@gmail.com>
2020-11-20 19:00:01 -0300

    Projeto Finalizado
    Projeto de lista de taréfas finalizado, fazendo agora  o backend para essa aplicação

---
+++ tasks/src/App.js
@@ -0,0 +1,24 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow strict-local
+ */
+
+import React from 'react';
+import {
+  View,
+ } from 'react-native';
+
+ import TaskList from './screens/TaskList';
+const App = () => {
+  return (
+    <>
+    <TaskList />
+    </>
+  );
+};
+
+
+export default App;
---
+++ tasks/src/screens/TaskList.js
@@ -0,0 +1,178 @@
+import React, { Component } from 'react';
+import {View,
+   Text,
+   ImageBackground,
+   StyleSheet,
+   FlatList,
+   TouchableOpacity,
+   Platform,
+   Alert,
+  } from 'react-native';
+
+  import moment from 'moment';
+  import 'moment/locale/pt-br';
+  import AsyncStorage from '@react-native-community/async-storage';
+  import Icon from 'react-native-vector-icons/FontAwesome';
+
+import commonStyles from '../commonStyles';
+import todayImage from '../../assets/imgs/today.jpg';
+import Task from '../components/Task';
+import AddTask from './Addtask';
+
+const initialState = {
+  showDoneTasks: true,
+    showAddTask: false,
+    visibleTask: [],
+    tasks: [],
+};
+
+export default class TaskList extends Component {
+
+  state = {
+    ...initialState,
+  }
+
+  componentDidMount = async () => {
+    // restaurar estado da aplicação no storage
+    const stateString = await AsyncStorage.getItem('tasksState');
+    const state = JSON.parse(stateString) || initialState;
+    this.setState(state, this.filterTasks);
+  }
+
+  toggleFilter = () => {
+    this.setState( { showDoneTasks: !this.state.showDoneTasks }, this.filterTasks );
+  }
+
+  filterTasks = () => {
+    let visibleTask = null;
+    if (this.state.showDoneTasks){
+      visibleTask = [...this.state.tasks];
+    } else {
+      const pending = task => task.doneAt === null;
+      visibleTask = this.state.tasks.filter(pending);
+    }
+    this.setState({visibleTask});
+    AsyncStorage.setItem('tasksState', JSON.stringify(this.state));
+  }
+
+toggleTask = taskId => {
+  const tasks = [...this.state.tasks];
+  tasks.forEach(task => {
+    if (task.id === taskId){
+      task.doneAt = task.doneAt ? null : new Date();
+    }
+  });
+
+  this.setState({tasks}, this.filterTasks);
+}
+
+addTask = (newTask) => {
+  if (!newTask.desc || !newTask.desc.trim()) {
+    Alert.alert('Dados Inválidos', 'Descrição não informada!');
+    return;
+  }
+
+  const tasks = [...this.state.tasks];
+  tasks.push({
+    id: Math.random(),
+    desc: newTask.desc,
+    estimateAt: newTask.date,
+    doneAt: null,
+  });
+
+  this.setState({tasks, showAddTask: false}, this.filterTasks);
+}
+
+deleteTask = id => {
+  const tasks = this.state.tasks.filter(task => task.id !== id);
+  this.setState({ tasks }, this.filterTasks);
+}
+
+  render() {
+    const today = moment().locale('pt-br').format('ddd, D [de] MMMM');
+    return (
+      <View style={styles.container}>
+        <AddTask isVisible={this.state.showAddTask}
+          onCancel={() => this.setState({showAddTask: false})}
+          onSave={this.addTask}
+        />
+        <ImageBackground source={todayImage}
+          style={styles.backGround}  >
+            <View style={styles.iconBar}>
+              <TouchableOpacity onPress={this.toggleFilter}>
+                <Icon name={this.state.showDoneTasks ? 'eye' : 'eye-slash'}
+                size={20} color={commonStyles.colors.secondary} />
+              </TouchableOpacity>
+            </View>
+           <View style={styles.titleBar}>
+             <Text style={styles.title}>Hoje</Text>
+             <Text style={styles.subTitle}>{today}</Text>
+           </View>
+         </ImageBackground>
+
+        <View style={styles.taskList}>
+          <FlatList data={this.state.visibleTask}
+            keyExtractor={item => `${item.id}`}
+            renderItem={({item}) => <Task {...item} onToggleTask={this.toggleTask} onDelete={this.deleteTask}/>}
+          />
+
+        </View>
+        <TouchableOpacity style={styles.addButton}
+          onPress={() => this.setState({showAddTask: true})}
+          activeOpacity={0.7}>
+          <Icon name="plus" size={20} color={commonStyles.colors.secondary} />
+        </TouchableOpacity>
+      </View>
+    );
+  }
+}
+
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  backGround: {
+    flex: 3,
+
+  },
+  taskList: {
+    flex: 7,
+  },
+  titleBar: {
+    flex: 1,
+    justifyContent: 'flex-end',
+  },
+  title: {
+    fontFamily: commonStyles.fontFamilly,
+    color: commonStyles.colors.secondary,
+    fontSize: 50,
+    marginLeft: 20,
+    marginBottom: 20,
+  },
+  subTitle: {
+    fontFamily: commonStyles.fontFamilly,
+    color: commonStyles.colors.secondary,
+    fontSize: 20,
+    marginLeft: 20,
+    marginBottom: 30,
+  },
+  iconBar: {
+    flexDirection: 'row',
+    marginHorizontal: 20,
+    justifyContent: 'flex-end',
+    marginTop: Platform.OS === 'ios' ? 40 : 10,
+  },
+  addButton: {
+    position: 'absolute',
+    right: 30,
+    bottom: 30,
+    width: 50,
+    height: 50,
+    borderRadius: 25,
+    backgroundColor: commonStyles.colors.today,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+
+});
